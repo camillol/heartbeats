3a1f5fd2f5292f5e51ca87c5e28713cd016be23f
Camillo Lugaresi <camillo.lugaresi@gmail.com>
2010-06-11 18:48:24 +0200

    fixed powerstates

--- src/powerstates.c
+++ src/powerstates.c
@@ -6,6 +6,14 @@
  *
  */
 
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/errno.h>
+#include <cpufreq.h>
+
+#define fail_if(exp, msg) do { if ((exp)) { fprintf(stderr, "%s:%d: %s: %s\n", __FILE__, __LINE__, (msg), strerror(errno)); goto fail; } } while (0)
+
 int get_core_count ()
 {
 	static int count = 0;
@@ -51,7 +59,7 @@ fail:
 /* square-and-multiply, lol */
 int ipow(int base, int exp)
 {
-	result = 1;
+	int result = 1;
 	while (exp) {
 		if (exp & 0x01) result *= base;
 		exp >>= 1;
@@ -63,7 +71,8 @@ int ipow(int base, int exp)
 #define SPEED_IDX 0
 #define POWER_IDX 1
 #define CORE_IDX(core) ((core) + 2)
-#define STATE_SIZE(core_count) (sizeof(unsigned long) * CORE_IDX(core_count))
+#define STATE_LEN(core_count) (CORE_IDX(core_count))
+#define STATE_SIZE(core_count) (sizeof(unsigned long) * STATE_LEN(core_count))
 
 /* completely made up! */
 static void calculate_state_properties(unsigned long *state, int core_count)
@@ -73,32 +82,33 @@ static void calculate_state_properties(unsigned long *state, int core_count)
 	
 	for (i = 0; i < core_count; i++) {
 		speed += state[CORE_IDX(i)];
-		power += (state[CORE_IDX(i)] > 0 ? 1000 : 0) + state[CORE_IDX(i)];
+		power += (state[CORE_IDX(i)] > 0 ? 1000000 : 0) + state[CORE_IDX(i)];
 	}
 	state[SPEED_IDX] = speed;
 	state[POWER_IDX] = power;
 }
 
-static void generate_machine_states_internal(unsigned long **states, unsigned long *state, int core_count, int freq_count, unsigned long *freq_array, int core)
+static void generate_machine_states_internal(unsigned long **state_cursor, unsigned long *state, int core_count, int freq_count, unsigned long *freq_array, int core)
 {
 	int i;
 	for (i = 0; i <= freq_count; i++) {
 		state[CORE_IDX(core)] = (i == freq_count ? 0 : freq_array[i]);
 		if (core == core_count - 1) {
-			calculate_state_properties(state);
-			memcpy(*states++, state, STATE_SIZE(core_count));
-		} else generate_machine_states_internal(states, state, core_count, freq_count, freq_array, core + 1);
+			calculate_state_properties(state, core_count);
+			memcpy(*state_cursor, state, STATE_SIZE(core_count));
+			(*state_cursor) += STATE_LEN(core_count);
+		} else generate_machine_states_internal(state_cursor, state, core_count, freq_count, freq_array, core + 1);
 	}
 }
 
 unsigned long *create_machine_states(int *state_count, int core_count, int freq_count, unsigned long *freq_array)
 {
-	unsigned long *states, *state;
+	unsigned long *states, *state, *statecursor;
 	
-	*state_count = ipow(freq_count, core_count)
+	*state_count = ipow(freq_count + 1, core_count);	/* we add freq 0 to represent the core being off */
 	state = malloc(STATE_SIZE(core_count));
-	states = malloc(STATE_SIZE(core_count) * *state_count);
-	generate_machine_states_internal(states, state, core_count, freq_count, freq_array, 0);
+	statecursor = states = malloc(STATE_SIZE(core_count) * *state_count);
+	generate_machine_states_internal(&statecursor, state, core_count, freq_count, freq_array, 0);
 	free(state);
 	return states;
 }
@@ -107,8 +117,9 @@ int compare_states_on_speed(unsigned long *a, unsigned long *b)
 {
 	if (a[SPEED_IDX] < b[SPEED_IDX]) return -1;
 	if (a[SPEED_IDX] > b[SPEED_IDX]) return 1;
-	if (a[POWER_IDX] < b[POWER_IDX]) return -1;
-	if (a[POWER_IDX] > b[POWER_IDX]) return 1;
+	/* if the speed is the same, sort by decreasing power */
+	if (a[POWER_IDX] > b[POWER_IDX]) return -1;
+	if (a[POWER_IDX] < b[POWER_IDX]) return 1;
 	return 0;
 }
 
@@ -118,14 +129,14 @@ int main(int argc, char **argv)
 	int core_count, freq_count;
 	unsigned long *freq_array;
 	int i, j;
-	unsigned long *states;
+	unsigned long *states, *state;
 	int state_count;
 
 	core_count = get_core_count();
 	freq_list = cpufreq_get_available_frequencies(0);
 	freq_count = create_freq_array(freq_list, &freq_array);
 	
-	states = create_machine_states(core_count, freq_count, freq_array);
+	states = create_machine_states(&state_count, core_count, freq_count, freq_array);
 	qsort(states, state_count, STATE_SIZE(core_count), compare_states_on_speed);
 	
 	printf("speed\tpower");
@@ -133,7 +144,7 @@ int main(int argc, char **argv)
 		printf("\tcore%d", j);
 	printf("\n");
 	
-	for (i = 0; i < state_count; i++) {
+	for (i = 0, state = states; i < state_count; i++, state+=STATE_LEN(core_count)) {
 		printf("%u\t%u", state[SPEED_IDX], state[POWER_IDX]);
 		for (j = 0; j < core_count; j++)
 			printf("\t%u", state[CORE_IDX(j)]);
