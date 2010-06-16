9197ae82d12853b40ab84322d746860995a1661f
Camillo Lugaresi <camillo.lugaresi@gmail.com>
2010-06-16 16:29:26 +0200

    added individual frequency actuators

--- src/combined.c
+++ src/combined.c
@@ -20,14 +20,15 @@
  */
 #define fail_if(exp, msg) do { if ((exp)) { fprintf(stderr, "%s:%d: %s: %s\n", __FILE__, __LINE__, (msg), strerror(errno)); goto fail; } } while (0)
 
-#define ACTUATOR_CORE 1
-#define ACTUATOR_FREQ 2
-#define ACTUATOR_COUNT 2
+#define ACTUATOR_CORE_COUNT 1
+#define ACTUATOR_GLOBAL_FREQ 2
+#define ACTUATOR_SINGLE_FREQ 3
 
 typedef struct actuator actuator_t;
 struct actuator {
 	int id;
 	pid_t pid;
+	int core;
 	int (*init_f) (actuator_t *act);
 	int (*action_f) (actuator_t *act);
 	uint64_t value;
@@ -165,35 +166,35 @@ int get_freq_index(freq_scaler_data_t *data, unsigned long freq)
 	return -1;
 }
 
-int freq_init (actuator_t *act)
+int single_freq_init (actuator_t *act)
 {
 	int err;
 	struct cpufreq_policy *policy;
 	struct cpufreq_available_frequencies *freq_list;
 	freq_scaler_data_t *data;
 	unsigned long freq_min, freq_max;
-
+	
 	act->data = data = malloc(sizeof(freq_scaler_data_t));
 	fail_if(!data, "cannot allocate freq data block");
-
-	err = cpufreq_get_hardware_limits(0, &freq_min, &freq_max);
+	
+	err = cpufreq_get_hardware_limits(act->core, &freq_min, &freq_max);
 	fail_if(err, "cannot get cpufreq hardware limits");
 	act->min = freq_min;
 	act->max = freq_max;
 	
-	policy = cpufreq_get_policy(0);
+	policy = cpufreq_get_policy(act->core);
 	fail_if(!policy, "cannot get cpufreq policy");
 	if (strcmp(policy->governor, "userspace") != 0) {
-		err = cpufreq_modify_policy_governor(0, "userspace");
-		policy = cpufreq_get_policy(0);
+		err = cpufreq_modify_policy_governor(act->core, "userspace");
+		policy = cpufreq_get_policy(act->core);
 		fail_if (strcmp(policy->governor, "userspace") != 0, "cannot set cpufreq policy to userspace");
 	}
 	
-	freq_list = cpufreq_get_available_frequencies(0);
+	freq_list = cpufreq_get_available_frequencies(act->core);
 	data->freq_count = create_freq_array(freq_list, &data->freq_array);
 	fail_if(data->freq_count < 1, "cannot get frequency list");
 	
-	act->value = act->set_value = cpufreq_get_freq_kernel(0);
+	act->value = act->set_value = cpufreq_get_freq_kernel(act->core);
 	data->cur_index = get_freq_index(data, act->value);
 	
 	return 0;
@@ -201,7 +202,28 @@ fail:
 	return -1;
 }
 
-int freq_act (actuator_t *act)
+int global_freq_init (actuator_t *act)
+{
+	int err;
+	
+	act->core = 0;	/* just get all data from the first cpu and assume they're all the same */
+	err = single_freq_init(act);
+	act->core = -1;
+	return err;
+}
+
+int single_freq_act (actuator_t *act)
+{
+	int err = 0;
+	
+	err = cpufreq_set_frequency(act->core, act->set_value);
+	/* warning: cpufreq_set_frequency tries sysfs first, then proc; this means that if
+	 sysfs fails with EACCESS, the errno is then masked by the ENOENT from proc! */
+	act->value = cpufreq_get_freq_kernel(act->core);
+	return err;
+}
+
+int global_freq_act (actuator_t *act)
 {
 	int err = 0;
 	int cpu;
@@ -226,7 +248,7 @@ void core_heuristics (heartbeat_record_t *current, int act_count, actuator_t *ac
 	static actuator_t *core_act = NULL;
 	int i;
 	for (i = 0; i < act_count && core_act == NULL; i++)
-		if (acts[i].id == ACTUATOR_CORE)
+		if (acts[i].id == ACTUATOR_CORE_COUNT)
 			core_act = &acts[i];
 	
 	if (current->window_rate < hrm_get_min_rate(&hrm)) {
@@ -243,7 +265,7 @@ void freq_heuristics (heartbeat_record_t *current, int act_count, actuator_t *ac
 	freq_scaler_data_t *freq_data;
 	int i;
 	for (i = 0; i < act_count && freq_act == NULL; i++)
-		if (acts[i].id == ACTUATOR_FREQ)
+		if (acts[i].id == ACTUATOR_GLOBAL_FREQ)
 			freq_act = &acts[i];
 	freq_data = freq_act->data;
 	
@@ -265,6 +287,11 @@ void uncoordinated_heuristics (heartbeat_record_t *current, int act_count, actua
 {
 	core_heuristics(current, act_count, acts);
 	freq_heuristics(current, act_count, acts);
+}
+
+void step_heuristics (heartbeat_record_t *current, int act_count, actuator_t *acts)
+{
+	/* TODO */
 }
 
 /* BACK TO ZA CHOPPA */
@@ -289,10 +316,8 @@ int main(int argc, char **argv)
 	int64_t skip_until_beat = 0;
 	int64_t last_beat = 0;
 	heartbeat_record_t current;
-	actuator_t controls[ACTUATOR_COUNT] = {
-		{ .id = ACTUATOR_CORE, .init_f = core_init, .action_f = core_act },
-		{ .id = ACTUATOR_FREQ, .init_f = freq_init, .action_f = freq_act }
-	};
+	int core_count, actuator_count;
+	actuator_t *controls;
 	decision_function_t decision_f;
 	int acted;
 
@@ -309,8 +334,17 @@ int main(int argc, char **argv)
 	printf("monitoring process %d\n", apps[0]);
 	
 	/* initrogenizing old river control structure */
-	for (i = 0; i < ACTUATOR_COUNT; i++) {
-		controls[i].pid = apps[0];
+	core_count = get_core_count();
+	actuator_count = core_count + 2;
+	
+	controls = malloc(sizeof(actuator_t) * actuator_count);
+	fail_if(!controls, "could not allocate actuators");
+	for (i = 0; i < core_count; i++)
+		controls[i] = (actuator_t) { .id = ACTUATOR_SINGLE_FREQ, .core = i,  .pid = -1,      .init_f = single_freq_init, .action_f = single_freq_act };
+	controls[i++] =   (actuator_t) { .id = ACTUATOR_GLOBAL_FREQ, .core = -1, .pid = -1,      .init_f = global_freq_init, .action_f = global_freq_act };
+	controls[i++] =   (actuator_t) { .id = ACTUATOR_CORE_COUNT,  .core = -1, .pid = apps[0], .init_f = core_init,        .action_f = core_act };
+	
+	for (i = 0; i < actuator_count; i++) {
 		err = controls[i].init_f(&controls[i]);
 		fail_if(err, "cannot initialize actuator");
 	}
@@ -330,17 +364,17 @@ int main(int argc, char **argv)
 
 		last_beat = current.beat;
 		if (current.beat < skip_until_beat) {
-			print_status(&current, skip_until_beat, '.', ACTUATOR_COUNT, controls);
+			print_status(&current, skip_until_beat, '.', actuator_count, controls);
 			continue;
 		}
 		
 		/*printf("Current beat: %lld, tag: %d, window: %lld, window_rate: %f\n",
 			   current.beat, current.tag, window_size, current.window_rate);*/
 		
-		decision_f(&current, ACTUATOR_COUNT, controls);
+		decision_f(&current, actuator_count, controls);
 		
 		acted = 0;
-		for (i = 0; i < ACTUATOR_COUNT; i++) {
+		for (i = 0; i < actuator_count; i++) {
 			actuator_t *act = &controls[i];
 			if (act->set_value != act->value) {
 				err = act->action_f(act);	/* TODO: handle error */
@@ -350,7 +384,7 @@ int main(int argc, char **argv)
 		}
 		skip_until_beat = current.beat + (acted ? window_size : 1);
 		
-		print_status(&current, skip_until_beat, acted ? '*' : '=', ACTUATOR_COUNT, controls);
+		print_status(&current, skip_until_beat, acted ? '*' : '=', actuator_count, controls);
 	}
 	
 	heart_rate_monitor_finish(&hrm);
